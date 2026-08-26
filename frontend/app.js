6c30d9bbab78fc0dc99a2ac0960accbae9152238
Nitya Upadhyay <nityaupadhyay1221@gmail.com>
2026-08-26 18:55:10 +0530

    added dropdown for categories

--- frontend/app.js
+++ frontend/app.js
@@ -118,6 +118,18 @@ async function deleteTransaction(id) {
     await loadAnalytics();
 }
 
+function setTodayDate() {
+    const today = new Date();
 
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+    document.getElementById("date").value = formattedDate;
+}
+
+setTodayDate();
 loadTransactions();
 loadAnalytics();
